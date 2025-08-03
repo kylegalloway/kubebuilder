c9f37aeabaee5b092786cf9d65b90e51cbfd1ed2
Kyle Galloway <kyle.galloway.1993@gmail.com>
2025-08-03 14:45:12 -0500

    Add basics of type

--- jobrunner/api/v1/leviathanbuild_types.go
+++ jobrunner/api/v1/leviathanbuild_types.go
@@ -1,44 +1,128 @@
-/*
-Copyright 2025.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1
 
 import (
+	// batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+// Important: Run "make generate" to regenerate code after modifying this file
+// The following markers will use OpenAPI v3 schema to validate the value
+// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
 
 // LeviathanBuildSpec defines the desired state of LeviathanBuild
 type LeviathanBuildSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-	// The following markers will use OpenAPI v3 schema to validate the value
-	// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
 
-	// foo is an example field of LeviathanBuild. Edit leviathanbuild_types.go to remove/update
+	// packageName is the name of the package being built/published
+	// +required
+	PackageName *string `json:"packageName"`
+
+	// buildType is the type of build
+	// - "Build" (default): runs a build of the given package;
+	// - "BuildPublish": runs a build and publish of the given package;
+	// - "Publish": runs a publish of the given package
 	// +optional
-	Foo *string `json:"foo,omitempty"`
+	// +kubebuilder:default:=Build
+	BuildType BuildType `json:"buildType,omitempty"`
+
+	// sourceType indicates the type of source that should be pulled from
+	// - "Local" (default): Use a local path for the source
+	// - "Git": Pull the source from git
+	// - "S3": Pull the source from an s3 bucket
+	// +optional
+	// +kubebuilder:default:=Build
+	SourceType SourceType `json:"sourceType,omitempty"`
+
+	// sourcePath indicates the path that the source should be pulled from
+	// +optional
+	SourcePath *string `json:"sourcePath,omitempty"`
+
+	// sourceURL indicates the URL that the source should be pulled from
+	// +optional
+	SourceURL *string `json:"sourceURL,omitempty"`
+
+	// // job defines the job that will be created when executing the given build.
+	// // +required
+	// Job batchv1.JobSpec `json:"job"`
+
+	// successfulJobsHistoryLimit defines the number of successful finished jobs to retain.
+	// This is a pointer to distinguish between explicit zero and not specified.
+	// +optional
+	// +kubebuilder:validation:Minimum=0
+	SuccessfulJobsHistoryLimit *int32 `json:"successfulJobsHistoryLimit,omitempty"`
+
+	// failedJobsHistoryLimit defines the number of failed finished jobs to retain.
+	// This is a pointer to distinguish between explicit zero and not specified.
+	// +optional
+	// +kubebuilder:validation:Minimum=0
+	FailedJobsHistoryLimit *int32 `json:"failedJobsHistoryLimit,omitempty"`
 }
+
+// BuildType describes how the job will be handled.
+// Only one of the following build types may be specified.
+// If none of the following types is specified, the default is build.
+// +kubebuilder:validation:Enum=Allow;Forbid;Replace
+type BuildType string
+
+const (
+	// Build runs a build of the given package
+	Build BuildType = "Build"
+
+	// BuildPublish runs a build of the given package, then publishes it
+	BuildPublish BuildType = "BuildPublish"
+
+	// Publish runs a publish of the given package
+	Publish BuildType = "Publish"
+)
+
+// SourceType indicates the type of source that should be pulled from
+// Only one of the following build types may be specified.
+// If none of the following types is specified, the default is local.
+// +kubebuilder:validation:Enum=Local;Git;S3
+type SourceType string
+
+const (
+	// Use a local path for the source
+	LocalSource SourceType = "Local"
+
+	// Pull the source from git
+	GitSource SourceType = "Git"
+
+	// Pull the source from an s3 bucket
+	S3Source SourceType = "S3"
+)
 
 // LeviathanBuildStatus defines the observed state of LeviathanBuild.
 type LeviathanBuildStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+
+	// active defines a list of pointers to currently running jobs.
+	// +optional
+	// +listType=atomic
+	// +kubebuilder:validation:MinItems=1
+	// +kubebuilder:validation:MaxItems=10
+	Active []corev1.ObjectReference `json:"active,omitempty"`
+
+	// lastJobTime defines when was the last time the job was successfully scheduled.
+	// +optional
+	LastJobTime *metav1.Time `json:"lastJobTime,omitempty"`
+
+	// For Kubernetes API conventions, see:
+	// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
+
+	// conditions represent the current state of the CronJob resource.
+	// Each condition has a unique type and reflects the status of a specific aspect of the resource.
+	//
+	// Standard condition types include:
+	// - "Available": the resource is fully functional
+	// - "Progressing": the resource is being created or updated
+	// - "Degraded": the resource failed to reach or maintain its desired state
+	//
+	// The status of each condition is one of True, False, or Unknown.
+	// +listType=map
+	// +listMapKey=type
+	// +optional
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
 }
 
 // +kubebuilder:object:root=true
